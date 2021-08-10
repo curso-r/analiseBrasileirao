44ecada89d95e1120463e94db571b4dbddb05be4
williamorim <thewilliam89@gmail.com>
2021-08-09 21:53:23 -0300

    baixei arquivo com os dados do brasileirao

---
+++ data-raw/brasileirao.R
@@ -0,0 +1,14 @@
+library(readr)
+
+# nome_arquivo <- paste0("data-raw/csv/brasileirao_", Sys.Date(), ".csv")
+
+download.file(
+  url = "https://raw.githubusercontent.com/williamorim/brasileirao/master/data-raw/csv/matches.csv",
+  destfile = "data-raw/csv/brasileirao.csv"
+  # destfile = nome_arquivo
+)
+
+brasileirao <- read_csv("data-raw/csv/brasileirao.csv")
+
+
+write_rds(brasileirao, "data/brasileirao.rds")
